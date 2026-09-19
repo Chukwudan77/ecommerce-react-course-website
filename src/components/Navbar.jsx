82673364bfd12528bfddca15a10e6e7da9635042
Daniel Eboagwu <daniel@gmail.com>
2026-09-19 16:10:56 +0100

    product details

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
+
 import { Link } from "react-router-dom"
+import {  useAuth } from "../context/AuthContext"
 
 export default function Navbar(){
+    const {user, logout}=useAuth()
     return <nav className="navbar">
         <div className="navbar-container">
         <Link to= "/" className="navbar-brand">
@@ -11,10 +14,15 @@ export default function Navbar(){
             <Link to="/checkout" className="navbar-link">Cart</Link>
         </div>
         <div className="navbar-auth">
-            <div className="navbar-auth-links">
+            {!user ? <div className="navbar-auth-links">
                 <Link to="/auth" className="btn btn-secondary">Login</Link>
                 <Link to="/auth" className="btn btn-primary">SignUp</Link>
-            </div>
+            </div>: (
+               <div className="navbar-user">
+                <span className="navbar-greeting">Hello,{user.email}</span>
+                <button className="btn btn-secondaer" onClick={logout}>Logout</button>
+               </div>
+            )}
 
         </div>
     </nav>
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,8 +1,8 @@
 
-import { createContext, useState } from "react";
+import { createContext, useState, useContext } from "react";
 
 
-export const AuthContext=createContext(null);
+const AuthContext=createContext(null);
 
 export default function AuthProvider({children}) {
     const[user, setUser]=useState(null);
@@ -41,5 +41,13 @@ export default function AuthProvider({children}) {
         setUser(null);
     }
     return <AuthContext.Provider value={{signUp, user, logout, login}}>{children}</AuthContext.Provider>
+}
+
+
+export function useAuth() {
+    const context=useContext(AuthContext)
+
+    return context;
+
 }
 
